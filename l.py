6ec03c3400792ccb69537690c329af3c4065a3dc
yarukha <fxdx@DESKTOP-87TABR6.localdomain>
2021-11-21 04:56:01 +0100

    calcul du l-vecteur

---
+++ l.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sys
+from r import calcul_r
+
+
+def calcul_l(u):
+    return calcul_r(u[::-1])[::-1]
+
+
+
+
+def main():
+    if len(sys.argv) !=2:
+        return("usage: r.py [word]")
+    else:
+        word=sys.argv[1]
+        return(calcul_l(word))
+
+if __name__== "__main__":
+    print(main())
+    
+
