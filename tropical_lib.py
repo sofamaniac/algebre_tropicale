3a3f812707d1ecb8dd3f148273d666e4bb0d2277
TheoRudkiewicz <azertyuiop9216012@gmail.com>
2021-11-23 15:53:43 +0100

    Ajoute de typage et suppression des opérations bit à bit dans power

--- tropical_lib.py
+++ tropical_lib.py
@@ -2,7 +2,8 @@
 import numpy as np
 
 
-def mut_mul(a, b):
+def mut_mul(a: np.array, b: np.array) -> np.array:
+    """Multiplie deux matrices dans l'alèbre tropicale min, +"""
     n = np.shape(a)[0]
     m = np.shape(b)[1]
     l = np.shape(b)[0]
@@ -10,14 +11,14 @@ def mut_mul(a, b):
     for i in range(n):
         for j in range(m):
             for k in range(l):
-                c[i, j] = min(c[i, j], a[i, k]+b[k, j])
+                c[i, j] = min(c[i, j], a[i, k] + b[k, j])
     return c
 
 
-def power(M, n):
+def power(m: np.array, n: int) -> np.array:
     if n == 1:
-        return M
-    elif n & 1 == 1:
-        return mut_mul(M, power(mut_mul(M, M), n >> 1))
+        return m
+    elif n % 2 == 1:
+        return mut_mul(m, power(mut_mul(m, m), n // 2))
     else:
-        return power(mut_mul(M, M), n >> 1)
+        return power(mut_mul(m, m), n // 2)
