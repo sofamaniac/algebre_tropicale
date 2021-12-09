ccfa8a967583ab28c5698da187659dc347efbda9
Qilowa <quentinsinh@gmail.com>
2021-12-09 18:56:30 +0100

    Calcul des cycles critiques et de la cyclité

--- scc.py
+++ scc.py
@@ -1,5 +1,8 @@
 import numpy as np
+import networkx as nx
 from matrice import calcul_matrix
+from math import gcd
+from functools import reduce
 import sys
 
 def tarjan(m):
@@ -45,8 +48,76 @@ def tarjan(m):
             strongConnected(i)
     return result
 
+def lcm(a, b):
+    return abs(a*b) // gcd(a, b)
+
+def critical_cycles(m):
+    """
+    Renvoie un dictionnaire associant chaque sous-composante connexe du 
+    graphe induit de la matrice à sa liste de cycle critique. Un cycle critique est représenté
+    par un doublet (liste des sommets du cycle, longueur du cycle)
+    """
+    a = np.copy(m)
+
+    a[a == np.inf] = 0
+
+    # On calcule les composantes connexes
+    components = tarjan(m)
+
+    scc_to_cycle = dict()
+
+    for c in components:
+        n = len(c)
+        m_ = np.full((n, n), 0)
+
+        for i in range(0, n):
+            for j in range(n):
+                m_[i, j] = a[c[i], c[j]]
+
+        g_scc = nx.convert_matrix.from_numpy_matrix(m_, create_using=nx.DiGraph)
+
+        dic = {}
+        for i in range(n):
+           dic[i] = c[i]
+        
+        # On détermine les cycles dans la composante fortement connexe
+        cycles = list(nx.simple_cycles(g_scc))
+        
+        min_weight = np.inf
+        critic = []
+        for cycle in cycles:
+            weight = 0
+            l = len(cycle)
+
+            for i in range(l):
+                weight += m_[cycle[i], cycle[(i+1) % l]]
+            
+            weight /= l
+            if weight < min_weight:
+                min_weight = weight
+                critic = [(cycle, l)]
+
+            elif weight == min_weight:
+                critic.append((cycle, l))
 
 
+        scc_to_cycle[tuple(c)] = critic
+
+    return scc_to_cycle
+
+def cyclicite(m):
+    """
+    Retourne la cyclité d'un graphe
+    """
+    cycles = critical_cycles(m)
+    l_gcd = []
+    for c, v in cycles.items():
+        if v != []:
+            pgcd = reduce(gcd, [lg for avg, lg in v])
+            l_gcd.append(pgcd)
+    
+    return reduce(lcm, l_gcd)
+    
 
 def main():
 
@@ -54,7 +125,12 @@ def main():
         exit(1)
 
     m = calcul_matrix(sys.argv[1])
-    print(tarjan(m))
+
+    cycles = critical_cycles(m)
+    for scc, c in cycles.items():
+        print(scc, c)
+    print(f"Cyclicité du mot {sys.argv[1]} : {cyclicite(m)}")
+
 
 if __name__ == "__main__":
     main()
