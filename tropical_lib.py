96c88cc114e3568a8533783462b99a0d3dcb09df
Theo <azertyuiop9216012@gmail.com>
2021-12-19 01:05:51 +0100

    Renomage de mut_mul en matrix_multiplication et modifications mineurs

--- tropical_lib.py
+++ tropical_lib.py
@@ -2,20 +2,26 @@
 import numpy as np
 
 
-def mut_mul(a: np.array, b: np.array) -> np.array:
+def matrix_multiplication(a: np.array, b: np.array) -> np.array:
     """Multiplie deux matrices dans l'alèbre tropicale min, +"""
-    n, ay = np.shape(a)
-    l, m = np.shape(b)
-    assert ay == l, "Dimensions compatible pour la multiplication"
+    assert a.ndim == 2, "Il faut des matrices à 2 dimensions"
+    assert b.ndim == 2, "Il faut des matrices à 2 dimensions"
+    n, ay = a.shape
+    l, m = b.shape
+    assert ay == l, \
+        f"Dimensions compatible pour la multiplication\nVous tenter de multiplier une matrice ({n, ay}) par une matice ({l, m})"
     c = np.full((n, m), np.Inf)
     for i in range(n):
         c[i] = (a[i] + b.transpose()).min(1)
     return c
 
+
 def power(m: np.array, n: int) -> np.array:
+    """Renvoie m^n"""
+    assert n >= 1, "n doit être supérieur à 1"
     if n == 1:
         return m
     elif n % 2 == 1:
-        return mut_mul(m, power(mut_mul(m, m), n // 2))
+        return matrix_multiplication(m, power(matrix_multiplication(m, m), n // 2))
     else:
-        return power(mut_mul(m, m), n // 2)
+        return power(matrix_multiplication(m, m), n // 2)
