f63a808c6b2c13f0c894f936e8578623b86fcd87
Theo <azertyuiop9216012@gmail.com>
2021-12-19 01:07:39 +0100

    Suppression de last_postion et utilisation de rfind sur le mot entier

--- r.py
+++ r.py
@@ -1,39 +1,23 @@
-import numpy as np
 import sys
-
-# returns the last position of a character c in an array of characters
-
-
-def last_position(t, c):
-    # we should work only on a prefix of the word uses in calcul_r
-    n = len(t)
-    # counter used to decrement
-    i = n-1
-    # last position of c, -1 by default
-    k = -1
-    while i >= 0 and k == -1:
-        if t[i] == c:
-            k = i
-        i -= 1
-    return k
+import numpy as np
 
 
-def calcul_r(u):
+def calcul_r(u: str) -> np.array:
     n = len(u)
     # we initialize the r vector
     r = np.zeros(n, dtype=int)
-    for i in range(0, n):
+
+    for i in range(n):  # calcul de r(u[:i], u[i])
         # for each letter c of the word u, and a prefix u' s.t. u=u'cu''
-        # we look at w, first suffic of u' starting with the letter c
-        c = u[i]
-        prefix = u[0:i]
-        # start_of_w=last_position(prefix,c)
-        start_of_w = prefix.rfind(c)
+        # we look at w, first suffix of u' starting with the letter c
+
+        # indice de la dernière occurence de u[i] dans u[:i]
+        start_of_w = u.rfind(u[i], 0, i)
 
         if start_of_w == -1:
             r[i] = 0
         else:
-            r[i] = 1+np.min(r[start_of_w:i])
+            r[i] = 1 + np.min(r[start_of_w:i])
     return r
 
 
