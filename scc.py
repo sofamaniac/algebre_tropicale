a6162d28dec573ab87def9b3d860b568963add33
Theo <azertyuiop9216012@gmail.com>
2021-12-23 12:08:40 +0100

    Implémentation de l'algorithme de Denardo

--- scc.py
+++ scc.py
@@ -54,19 +54,18 @@ def critical_cycles(m: np.array):
         critic = []
         for cycle in cycles:
             weight = 0
-            l = len(cycle)
 
             # Addition des poids d'un cycle
-            for i in range(l):
-                weight += m_[cycle[i], cycle[(i + 1) % l]]
+            for i in range(len(cycle)):
+                weight += m_[cycle[i], cycle[(i + 1) % len(cycle)]]
 
-            weight /= l
+            weight /= len(cycle)
             if weight < min_weight:
                 min_weight = weight
-                critic = [(cycle, l)]
+                critic = [(cycle, len(cycle))]
 
             elif weight == min_weight:
-                critic.append((cycle, l))
+                critic.append((cycle, len(cycle)))
 
         scc_to_cycle[tuple(c)] = critic
 
@@ -76,18 +75,53 @@ def critical_cycles(m: np.array):
 def cyclicite(m: np.array):
     """
     Retourne la cyclité d'un graphe
+    {SCC: [([Sommets composant le cycle1], longueur du cycle1), ([Sommets composant le cycle2], longueur du cycle2) ...]}
     """
-    # {SCC: [([Sommets composant le cycle1], longueur du cycle1), ([Sommets composant le cycle2], longueur du cycle2) ...]}
     scc = critical_cycles(m)
     l_gcd = []
     for c, v in scc.items():
-        if v != []:
+        if v:
             # On applique le PGCD sur l'ensemble des cycles d'une SCC pour trouver la cyclicité de la composante connexe
-            pgcd = reduce(gcd, [lg for avg, lg in v])
+            pgcd = reduce(gcd, (lg for _, lg in v))
             l_gcd.append(pgcd)
 
     # On applique le PPCM sur la cyclicité de chaque composante connexe du graphe
     return reduce(lcm, l_gcd)
+
+
+def denardo_algorithm(graph: np.array, component: list):
+    n = graph.shape[0]
+    v_tree = -np.ones(n, dtype=np.int64)
+    tree = np.zeros((n, n), dtype=bool)
+
+    # calcul de l'arbre couvrant
+    s = component[0]
+    nb_to_do = len(component) - 1
+    v_tree[s] = 0
+    to_explore = [(s, v) for v in range(n) if graph[s, v] != np.inf]
+    while nb_to_do > 0:
+        s, v = to_explore.pop()
+        if v_tree[v] != -1:
+            v_tree[v] = v_tree[s] + 1
+            tree[s, v] = True
+            to_explore.extend([(v, w) for w in range(n) if graph[v, w] != np.inf])
+            nb_to_do -= 1
+
+    current_gcd = 0
+    for i in component:
+        for j in component:
+            if graph[i, j] and not tree[i, j]:
+                kij = v_tree[i] - v_tree[j] + 1
+                current_gcd = gcd(current_gcd, kij)
+    return current_gcd
+
+
+def denardo_period(m: np.array):
+    scc = strongly_connected_components(m)
+    lscc = [(len(c), i) for i, c in enumerate(scc)]
+    lscc.sort()
+    assert all(length == 1 for length, _ in lscc[:-1]), "Il y a visiblement plus d'une composante connexe non triviale"
+    return denardo_algorithm(m, scc[lscc[-1][1]])
 
 
 def main():
